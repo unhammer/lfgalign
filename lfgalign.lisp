cdbf16adb311261b8ddcad6069706651661b9fbb
Kevin Brubeck Unhammer <p.ixiemotion@gmail.com>
2010-01-29 23:19:29 +0100

    remove fluff

--- lfgalign.lisp
+++ lfgalign.lisp
@@ -6,16 +6,3 @@
 
 ;;; Giza++ gives us the LPT-correspondence (criterion i), our Prolog
 ;;; files should have the information to use criterion ii.
-
-(defparameter f-parse
-  nil
-  "The f-parse of the Prolog file.")
-(defparameter c-parse
-  nil
-  "The c-parse of the Prolog file.")
-
-
-(with-open-file
-    (stream (merge-pathnames "dev/TEST_parse.pl"
-			     (asdf:component-pathname (asdf:find-system :lfgalign))))
-  (cdr (parse-pred stream)))
