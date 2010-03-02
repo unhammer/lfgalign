094297ca57d1822f645807ae517ff1a841294e11
Kevin Brubeck Unhammer <p.ixiemotion@gmail.com>
2010-03-02 10:24:01 +0100

    Disjoint sets with path compression and union by rank, for holding the eq vars. Since we also need to find all elements of the equivalence classes, we collect (circular) backreferences to the children each time we set a new parent/root. There must be a better way to do this, but at least this works (early optimisation etc.)

--- cl-extra.lisp
+++ cl-extra.lisp
@@ -20,3 +20,65 @@ http://www.cliki.net/common-idioms"
      (if it ,conseq
          (macrolet ((setf-it (val) (list 'setf ',test val)))
            ,else))))
+
+;;; Disjoint set implementation. Remember to (setf *print-circle* t)
+;;; if you want to use this! Also, to accumulate child-vals, don't set
+;;; `dset-parent', but use `dset-setparent' instead.
+(defstruct dset parent val (rank 0) child-vals)
+
+(defun dset-find (d)
+  "Find the root"
+  (aif (dset-parent d)
+       (setf (dset-parent d) (dset-find it))
+       d))
+
+(defun dset-setparent (child parent)
+  (setf (dset-parent child) parent)
+  (setf (dset-child-vals parent) (union
+ 				  (cons child
+					(dset-child-vals child))
+ 				  (dset-child-vals parent)))
+  (setf (dset-child-vals child) nil))
+
+(defun dset-union (d1 d2)
+  "Destructively union `d1' and `d2', returning the new root."
+  (let* ((root1 (dset-find d1))
+	 (root2 (dset-find d2))
+	 (rank1 (dset-rank root1))
+	 (rank2 (dset-rank root2)))
+    (cond
+      ((> rank1 rank2) (dset-setparent root2 root1))
+      ((< rank1 rank2) (dset-setparent root1 root2))
+      ((not (eq root1 root2))
+       (dset-setparent root2 root1)
+       (incf (dset-rank root1))
+       root1)
+      (t root1))))
+
+(defun dset-collect (equivs)
+  "Return a table where keys are f-structure variable numbers, values
+are dsets representing the equivalence classes. Runs on the |eqvar|
+alist from `dup-alist-to-table'. Non-numbers are not added, since
+their hashes are not unique."
+  (labels ((addnew (val tab)
+	     (aif (gethash val tab)
+		  it
+		  (if (numberp val)
+		      (setf (gethash val tab) (make-dset :val val))
+		      (make-dset :val val)))))
+    
+      (loop for (aval . bval) in equivs
+	 with dsets = (make-hash-table)
+	 for aset = (addnew aval dsets)
+	 for bset = (addnew bval dsets)
+	 do (dset-union bset aset)
+	 finally (return dsets))))
+
+(defun dset-findall (val dsets)
+  "Return all dsets in the same set as `val' in the hash table
+`dsets'."
+  (aif (gethash val dsets)
+       (let ((root (dset-find it)))
+	 (mapcar #'dset-val
+		 (cons root (dset-child-vals root))))))
+
